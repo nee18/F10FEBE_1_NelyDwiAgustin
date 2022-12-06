7bae94468fe98163d261e03b4112ef70c0f84054
nee18 <99226926+nee18@users.noreply.github.com>
2022-12-06 18:16:23 +0700

    Submission: Tugas Akhir Membuat Website

---
+++ README.md
@@ -0,0 +1,1 @@
+# F10FEBE_1_NelyDwiAgustin
---
+++ assets/styles/DOM.js
@@ -0,0 +1,9 @@
+console.log(`ini Front End Web pertama saya, semoga bermanfaat dan dapat mengembangkan hard skill pada bidang programmer saya
+Nama              : Nely Dwi Agustin
+SIB ID            : F260Y0582
+SIB Group         : F10
+Kelas             : Belajar Dasar Pemrograman Web
+NIM               : 19533133 
+Asal Universitas  : Universitas Muhammadiyah Ponorogo 
+Asal Jurusan      : Teknik Informatika 
+Dosen Pembimbing  : Adi Fajaryanto Cobantoro, S.Kom., M.Kom. `);
